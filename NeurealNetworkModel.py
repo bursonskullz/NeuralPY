c69a0392f4ea4343a1ad6674165bf548469a3349
ROY BURSON <159820187+bursonskullz@users.noreply.github.com>
2025-09-17 22:46:27 -0700

    Update NeurealNetworkModel.py
    added gradient(), collectDataTotrain(), trainModel() and comploss()

--- NeurealNetworkModel.py
+++ NeurealNetworkModel.py
@@ -1,13 +1,15 @@
 '''
-Date 9-16-25
+Date 9-17-25
 Author: Roy Burson
 Purpose: 1) Create a python class that utilizes a neural network which can train AI agents and models for different task. 
          2) Gain experience with the concepts and mathematical nature of the training process. 
          3) Engage into machine learning algorithms to ehance my skills.  
 '''
 
+
 import numpy as np
 from numpy import ndarray
+import torch as tor # use built in gradient method
 
 class Neural:
 	Base = {chr(i): i-31 for i in range(32,127)}
@@ -49,22 +51,58 @@ class Neural:
 			H = getNetworkLayer(weightedMatrix, H, bias)
 		return lambda X: activation(np.dot(weightedMatrix, X.T) + bias)
 
+	def comploss(predicted: ndarray,  true_value: ndarray, beta = 1e-12) -> float:
+		# loss function using a ratio 
+		return np.mean((1- predicted/(true_value+beta))**2)
 
+	def gradient(lossFunction, weightedMatrix, bias, X, Y_true):
+		weightedMatrix = tor.tensor(weightedMatrix, requires_grad = True)
+		biases = tor.tensor(bias, requires_grad = True)
+		y_pred = tor.relu(tor.matmul(weightedMatrix, X.T)+biases)
+		l = lossFunction(y_pred, y_true)
+		l.backward()
+		partialWeight = weightedMatrix.grad.numpy()
+		partialBias = bias.grad.numpy()
+		return partialWeight, partialBias
 
 #----------------------Functions to train and define W, and B below -----------------------------
 
 	def collectDataTotrain():
-		# collect data that will be used to train the model
-		print('use a large set of bad responses')
+		# this function can be altered to return data relevent to the problem
+		# i.e for movie reviews the question will be about movies and the answer will be bad or good
+		return [
+			("Is the movie bad?", "It was to long!"),
+			("How was the movie?", "Long and boring!"),
+			("Did you enjoy the movie?", "No it was not cool!"),
+			("How long was the movie?", "It was to long!"),
+			("Was it annoying?", "It was not enjoyable!"),	
+		]
 
-	def trainModel(numberOfLayers):
-		neuralNetwork = computeNeuralNetwork(numberOfLayers) # returns the network
-		# use this nueral network to perform perform mimimization techniques and train the model
-		# need some data to train it that collectDataToTrain() will intatiate
+	def trainModel(dataSet, trainingSteps = 50 , learningRate = 0.01):
 		print('calling to train the model')
-	
-	"""
-	if __name__ == 'main':
-		collectDataTotrain()
-		trainModel()
-	"""
+		neuralNetwork = computeNeuralNetwork(numberOfLayers) 
+		for step in range(trainingSteps):
+			tLoss = 0
+			for prompt, y_true in dataSet:
+				numericalPrompt = convertPromptToNumericalForm(prompt)
+				embededPrompt = getembeddedVector(numericalPrompt, embedAccuracy)
+				res = np.dot(weightedMatrix, embededPrompt.T) + bias
+				prediction = activation(res)
+				lossFunction = comploss(prediction, y_true)
+				tLoss += loss
+				gradientW, gradientB = gradient(prediction, y_true, embededPrompt, res)
+				weightedMatrix -= learningRate * gradientW
+				bias -= learningRate * gradientB 
+			print(f"training step {step}, AVG_Loss = {tLoss/len(dataSet)}")
+		
+
+#-----------------------------------------implementation-----------------------------------------
+if __name__ == '__main__':
+	print('main called')
+	net = Neural()
+	data = net.collectDataTotrain()
+	trainModel(data, 75, 0.025)
+	question = 'was the movie good?'
+	prediction = net.computeNeuralNetwork(1, question)
+	result = net.decode_answer(prediction)
+	print(f"result: {result}")
